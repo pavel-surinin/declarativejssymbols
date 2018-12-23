fb53574fb9414f5a2dfb52ff912742f583f3ff3b
pavel-surinin <pavel.surinin@gmail.com>
2018-12-23 07:58:42 +0200

    test commit

---
+++ src/Array.ts
@@ -0,0 +1,25 @@
+import { a } from './Symbol'
+
+declare global {
+    interface Array<T> {
+        [a](): {
+            /**
+             * docs
+             */
+            present(): Array<NonNullable<T>>
+            /**
+             * docs unique by
+             */
+            uniqueBy<K extends keyof T>(key: K): Array<T>
+            uniqueBy<K extends keyof T>(key: StringGetter<T>): Array<T>
+            /**
+             * docs1
+             */
+            toObject<K>(key: StringGetter<T>, value: Getter<T, K>): Indexed<T>
+            /**
+             * docs2
+             */
+            toObject<K>(key: StringGetter<T>): Indexed<T>
+        }
+    }
+}
